785a9d0bfa9739a4c296e9d2bbe6d9cf5d41f0c2
Jayasankari_Raajkomahan <shanksankari@gmail.com>
2024-04-12 23:52:46 +0530

    third

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://e-shop-consultancy-ez1o.vercel.app/',
   credentials: true
 }));
 
